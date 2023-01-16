5f682250ced23d203e970bd9c6265701a0de2d46
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-01-16 14:35:20 +0000

    checked if we are on server-side or client-side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/Layout/Navbar'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { useState, useEffect } from 'react'
 
-const queryClient = new QueryClient;
+export const queryClient = new QueryClient;
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setISSR] = useState(true);
+
+  useEffect(() => {
+    setISSR(false)
+  }, [])
+  if (isSSR) return null;
+
   return (
-  <>
-  <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools position='bottom-right'/>
-  <Navbar/>
-  <Component {...pageProps} />
-  </QueryClientProvider>
-  </>
- )
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ReactQueryDevtools position='bottom-right' />
+          <Navbar />
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </>
+  )
 }
+
+
+export default App;
